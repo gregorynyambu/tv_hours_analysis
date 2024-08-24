05f405e2c364c2b23199f089bfa43aad7289e00f
gregorynyambu <gregorymwandau@gmail.com>
2024-08-24 11:47:17 +0300

    filtered to those 30 and above

--- main.R
+++ main.R
@@ -1,14 +1,9 @@
 library(tidyverse)
 library(here)
 
-gss_cat %>% 
-  group_by(marital) %>% 
-  summarise(mean_tv_hours = mean(tvhours, na.rm = T))
-
 tv_hours_table <- gss_cat %>% 
+  filter(age >=30)
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = T))
 
-install.packages("here")
-
-write.csv(tv_hours_table, here("TV_Hours_by_Marital.csv"))
+  write.csv(tv_hours_table, here("TV_Hours_by_Marital.csv"))
